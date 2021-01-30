10ad6f18f95e5d881832000ab4259731274924bf
ldco2016 <renaissance.scholar2012@gmail.com>
2021-01-30 13:11:05 -0600

    Updated yml container file and App.js

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Howdy!</h1>
+      <h1>Howdy Yall!</h1>
       <hr />
       <MarketingApp />
     </div>
